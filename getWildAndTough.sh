bee4359fdac302e7165d967506308488efca23fd
cospa-fujiken <fujiwara.kenta@cospa-creation.co.jp>
2017-07-20 17:16:57 +0900

    GetWildAndでToughを出力する

--- getWildAndTough.sh
+++ getWildAndTough.sh
@@ -6,11 +6,9 @@ AND="and"
 TOUGH="Tough"
 
 getWildAndTough=($GET $WILD $AND $TOUGH)
-getChanceAndLuck=(0 0 0)
-g_flg=false
-w_flg=false
-a_flg=false
+getChanceAndLuck=(9 9 9)
 
+# 途中でキャンセルするための処理
 function last () {
   status=$?
   echo '強制終了しました'
@@ -18,32 +16,60 @@ function last () {
   echo  in trap, status captured
   exit $status
 }
-
 trap 'last'  {1,2,3,15}
 
+# 初期化
+dream=false
 
-#for (( i = 0; i < ${#getWildAndTough[@]}; ++i )); do
-
+# 無限ループ
 while true; do
+
+  # ランダムに１～３の数値を取得
   random=$(( (RANDOM % 3) + 1 ))
+
+  # 数値を配列用に０～２に変換
   rnd=$(( random - 1 ))
+  
+  # 文字列を取得
   echo ${getWildAndTough[$rnd]}
 
-  getChanceAndLuck[$rnd]=${getWildAndTough[$rnd]}
+  # 一つ目の文字列を格納
+  if test ${getChanceAndLuck[0]} -eq 9 ; then
+    getChanceAndLuck[0]=$rnd
+  else
 
-  # IFSバックアップ
-  IFS_BACKUP="$IFS"
-  # 配列の区切り文字を改行に変更
-  IFS=$'\n' 
-  both=(`{ echo "${getWildAndTough[*]}" ; echo "${getChanceAndLuck[*]}"; } | sort | uniq -u`) 
-  # IFSを元に戻す
-  IFS="$IFS_BACKUP"
-  # IFS_BACKUP変数を削除
-  unset IFS_BACKUP
+    # 二つ目の文字列を格納
+    if test ${getChanceAndLuck[1]} -eq 9 ; then
+      getChanceAndLuck[1]=$rnd
 
-  if [ $both = "Tough" ] ; then
+    # 三つ目の文字列を格納（以降、毎回三つ目に格納して判定する）
+    else
+      getChanceAndLuck[2]=$rnd
+    fi
+  fi
+
+  # 判定条件「０，１，２」
+  if test ${getChanceAndLuck[0]} -eq 0 && test ${getChanceAndLuck[1]} -eq 1 && test ${getChanceAndLuck[2]} -eq 2 ; then
+    dream=true
+
+  # 判定NGの場合
+  else
+    dream=false
+
+    # 3要素の合計が６以下の場合
+    if [ $(( ${getChanceAndLuck[0]} + ${getChanceAndLuck[1]} + ${getChanceAndLuck[2]} )) -le 6 ] ; then
+
+      # 各要素を左にシフトする（三つ目を空ける）
+      getChanceAndLuck[0]=${getChanceAndLuck[1]}
+      getChanceAndLuck[1]=${getChanceAndLuck[2]}
+    fi
+  fi
+
+  # 判定OKの場合ループを抜ける
+  if [ $dream = true ] ; then
     echo ${getWildAndTough[3]}
     break;
   fi
+
 done
 
